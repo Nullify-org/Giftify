ffa0492e1b012f75fc42d3ab9ec057b823f1a341
Nour-CS19 <Nourmaher7791@gmail.com>
2026-05-07 19:13:24 +0300

    fix by noureddine

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,6 +7,10 @@ namespace Giftify.Data;
 
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+    {
+    }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<ProductImage> ProductImages { get; set; }

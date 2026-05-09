294b062cdd5f3562eb756230d66063cc1bcea69b
Mohammed Ashraf <muhamiidashraf22@gmail.com>
2026-05-09 16:55:13 +0300

    Resolved conflicts in UnitOfWork

--- Interfaces/IUnitOfWork..cs
+++ Interfaces/IUnitOfWork..cs
@@ -4,17 +4,10 @@ namespace Giftify.Interfaces;
 
 public interface IUnitOfWork : IDisposable
 {
-<<<<<<< HEAD
     ICartRepository Carts { get; }
     IOrderRepository Orders { get; }
     IProductRepository Products { get; }
     IOccasionRepository Occasions { get; }
-=======
-     ICartRepository Carts { get; }
-     IOrderRepository Orders { get; }
-     IProductRepository Products { get; }
-
->>>>>>> 1e4876017e0475229b7cb5fd3cb178f81b5991d5
     ICategoryRepository Categories { get; }
     Task<int> Save();
     Task SaveChangesAsync();

c7a4d7efa239c1d771c99db9801e146ec35ea43d
Mohammed Ashraf <muhamiidashraf22@gmail.com>
2026-05-09 16:56:34 +0300

    Resolved conflicts in CategoryRepository Interface

--- Interfaces/Repositories/ICategoryRepository.cs
+++ Interfaces/Repositories/ICategoryRepository.cs
@@ -3,52 +3,7 @@ using System.Linq.Expressions;
 
 namespace Giftify.Interfaces.Repositories
 {
-<<<<<<< HEAD
     public interface ICategoryRepository : IBaseRepository<Category>
     {
-      
-=======
-    public class ICategoryRepository : IBaseRepository<Category>
-    {
-        public Task AddAsync(Category entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(Category entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Category>> FindAllAsync(Expression<Func<Category, bool>> criteria, params Expression<Func<Category, object>>[]? includes)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Category?> FindAsync(Expression<Func<Category, bool>> criteria, params Expression<Func<Category, object>>[]? includes)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Category?> FindAsync(Expression<Func<Category, bool>> criteria, string[] includes = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Category>> GetAllAsync(params Expression<Func<Category, object>>[]? includes)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Category?> GetByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Category entity)
-        {
-            throw new NotImplementedException();
-        }
->>>>>>> 1e4876017e0475229b7cb5fd3cb178f81b5991d5
     }
 }

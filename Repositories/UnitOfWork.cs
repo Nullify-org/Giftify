b5ee9cbdfb86c1033a4f32f3b1ef523628cdb356
Mohammed Ashraf <muhamiidashraf22@gmail.com>
2026-05-09 17:06:10 +0300

    Resolved Conflicts in UnitOfWork

--- Repositories/UnitOfWork.cs
+++ Repositories/UnitOfWork.cs
@@ -14,47 +14,26 @@ public class UnitOfWork : IUnitOfWork
     public IOccasionRepository Occasions { get; private set; }
     public ICategoryRepository Categories { get; private set; }
 
-<<<<<<< HEAD
     public UnitOfWork(ApplicationDbContext context, ICartRepository cartRepo,
                         IOrderRepository orderRepo, IProductRepository productRepo,
                         ICategoryRepository categoryRepo, IOccasionRepository occasions)
-=======
-    public ICategoryRepository Categories { get; private set; }
 
-    public UnitOfWork(ApplicationDbContext context, ICartRepository cartRepo, 
-                        IOrderRepository orderRepo, IProductRepository productRepo , ICategoryRepository categoryRepo)
->>>>>>> 1e4876017e0475229b7cb5fd3cb178f81b5991d5
     {
         this._context = context;
         Carts = cartRepo;
         Orders = orderRepo;
         Products = productRepo;
         Categories = categoryRepo;
-<<<<<<< HEAD
         Occasions = occasions;
     }
 
-
-
-=======
-        
-    }
-
-    
-    
->>>>>>> 1e4876017e0475229b7cb5fd3cb178f81b5991d5
     public void Dispose()
     {
         _context?.Dispose();
     }
 
-    public Task<int> Save()
+    public async Task<int> Save()
     {
-        throw new NotImplementedException();
-    }
-
-    public Task SaveChangesAsync()
-    {
-        throw new NotImplementedException();
+        return await _context.SaveChangesAsync();
     }
 }

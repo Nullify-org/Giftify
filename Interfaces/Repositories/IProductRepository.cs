bba75cf83fff59a5aff8f6626c72ebd364105c1b
Mohammed Ashraf <muhamiidashraf22@gmail.com>
2026-05-09 16:55:38 +0300

    Resolved conflicts in Product Repository

--- Interfaces/Repositories/IProductRepository.cs
+++ Interfaces/Repositories/IProductRepository.cs
@@ -5,10 +5,5 @@ namespace Giftify.Interfaces.Repositories;
 
 public interface IProductRepository : IBaseRepository<Product>
 {
-<<<<<<< HEAD
     Task <IEnumerable<Product>> SearchAsync(ProductFilterVM model);
 }
-=======
-
-}
->>>>>>> 1e4876017e0475229b7cb5fd3cb178f81b5991d5
